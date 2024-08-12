d66086961d6dd015c367daa951c24c77a1b110d9
Andrea Rethy <125359387+AndreaRethy@users.noreply.github.com>
2024-08-12 11:10:47 +0200

    Update script.js

--- Exercici1/script.js
+++ Exercici1/script.js
@@ -1,19 +1,30 @@
 const mongoose = require('mongoose');
-const Customer = require("./schemas/Customer")
+const Customer = require("./schemas/Customer");
+const Glasses = require('./schemas/Glasses');
 
 mongoose.connect("mongodb://localhost/opticadb");
 
 run()
 async function run() {
     try {
-        const user = await Customer.create({
-            name: "Andrea",
-            age: 33,
-            email: "andi@mail.com",
-        })
-        // const user = new User({ name: "Andrea", age: 33 })
-        // await user.save()
-        console.log(user)
+        const glasses = await Glasses.create({
+            brand: "Rayban",
+            prescription_right: 2.0,
+            prescription_left: 2.3,
+            type_of_frame: 'metàl·lica',
+            price: 127.0
+        });
+        const customer = await Customer.create({
+            name: "Client Name",
+            address: {
+                street: "Right Street",
+                number: 123,
+            },
+            email: "email@email.com",
+            purchases: [glasses._id]
+        });
+        console.log(glasses);
+        console.log(customer);
     } catch (e) {
         console.log(e.message)
     }
